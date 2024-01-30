d7210b9833928ac6b3321f50f7a8c4c8315dcacd
Andrew Pikul <ajpikul@gmail.com>
2024-01-29 22:51:24 -0500

    Generate Graph: WARNING:
    Strangely, this file no longer seems to run properly in the firefox dev
    console. That's unforunate, but I'll try and move it to a static file.

--- refactor/analyze-webpack-firefox-console-script.js
+++ refactor/analyze-webpack-firefox-console-script.js
@@ -7,9 +7,10 @@ var allRun = false;
 
 
 var normalPath = new RegExp(/^\.\/.*(\.js|\.ts|\.mjs|\.cjs)$/);
-
+console.log("here")
 // toRun returns true if variable is not set properly. Wrap your variable in an object so we can get its variable name.
 function toRun(variable) {
+  console.log(`Checking ${Object.keys(variable)}`)
   const access = Object.values(variable)[0];
   if(allRun) {
     console.log(`Setting ${Object.keys(variable)}`)
@@ -142,7 +143,7 @@ all_reasons.forEach((reason) => {
   }
 });
 
-var self_referring_types = true;
+var self_referring_types;
 var weirdNames;
 var weirdResolvedModule2;
 if (toRun({self_referring_types}) ||
@@ -167,9 +168,93 @@ if (toRun({self_referring_types}) ||
 }
 console.log(weirdNames)
 console.log(weirdResolvedModule2);
+// these two are good, but it will still double some things up
 console.log(self_referring_types);
+// we also have module_by_name to work with
+const cjs_types = ["cjs export require", "cjs full require", "cjs require", "cjs self exports reference"];
+const entry_types = ["entry"];
+const harmony_types = ["harmony export imported specifier", "harmony import specifier", "harmony side effect evaluation"];
+const provided_types = ["harmony export imported specifier"];
+// ignore null
+class Edge {
+  is_a;
+  of;
+  cjs;
+  entry;
+  harmony;
+  provided;
+	constructor(is_a, of, cjs, harmony, entry, provided) {
+    this.is_a = is_a;
+    this.of = of;
+    this.cjs = cjs;
+    this.entry = entry;
+    this.harmony = harmony;
+    this.provided = provided;
+  }
+}
+class Dependent extends Edge {}
+class Dependency extends Edge {}
+class ModuleNode {
+  name;
+  depth;
+  module;
+  dependents;
+  dependencies;
+  contructor(name) {
+  	console.log("here")
+    this.name = name;
+    this.depth = null;
+    this.module = null;
+    this.dependents = new Array();
+    this.dependencies = new Array();
+    console.log(`${name} initialized!`)
+  }
+}
+var module_index = false;
+if(toRun({module_index})) {
+  module_index = {};
+  for (let type in all_reason_types) {
+    all_reason_types[type].forEach((tuple) => {
+      var holding = tuple["holdingModule"];
+      var reason = tuple["reason"];
+      if (!normalPath.test(holding.name) || !normalPath.test(reason.resolvedModule)) {
+				return;
+  		}
+      if (module_index[holding.name] === undefined) {
+        module_index[holding.name] = new ModuleNode(holding.name);
+      } 
+      var holdingMod = module_index[holding.name];
+      console.log(holdingMod)
+      holdingMod.depth = holding.depth;
+      holdingMod.module = holding;
+      holdingMod.dependents.push(new Dependent(reason.resolvedModule, holding.name,
+                                 							 cjs_types.contains(reason.type),
+                                               harmony_types.contains(reason.type),
+                                               entry_types.contains(reason.type),
+                                               provided.contains(reason.type)));
+      if (module_index[reason.name] === undefined) {
+        module_index[reason.name] = new ModuleNode(reason.resolvedModule);
+      }
+      var reasonMod = module_index[reason.resolvedModule];
+      reasonMod.dependencies.push(new Dependency(olding.name, reason.resolvedModule,
+                                 							 cjs_types.contains(reason.type),
+                                               harmony_types.contains(reason.type),
+                                               entry_types.contains(reason.type),
+                                               provided.contains(reason.type)));
+      
+    });
+  }
+} // search all for duplicate reasons
 
-// Alright, lets check type mixes
+
+// we'll create a dictionary of names to relationships
+// relationship is two types, dependants, dependencies
+// relationship will also have several properties provided above
+// after that we have to create a tree of paths
+// selected parts of the tree wil generate the whole node graph
+// well match names to their proper path
+
+
 
 console.log(inconsistentReasons);
 console.log(weirdResolvedModule);
@@ -181,19 +266,10 @@ console.log(reason_counts_by_depth); // Interesting to graph, better w/o filters
 
 // key point: development vs produciton build, do both require debug. Who is calling in debug?
 
-// How many types have module names look at that? (maybe we can push reasons)
-// What's the unique set of module names? match mod_by_file.length?
-// Redo the above but with unique module names
-
-// Identify if it's cjs or ESM
-
-
 // Get all modules for which I'm a reason
 
-// let's create a directory tree
-// let's create a depth tree
-// process reasons
 // folders with index.js
 // folders w/ no index.js, all files
+// measure tanglemant by how many people skip to my immediate folder
 
 "done"
